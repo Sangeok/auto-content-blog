3d8c0cc456f5a7b0f757d637208a9ae147a8fafd
HamSangeok <hamsoo159@naver.com>
2024-03-05 16:52:52 +0900

    image upload with url

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -15,12 +15,15 @@ const FileUpload = ({
     value,
 } : FileUploadProps) => {
     const [file, setFile] = useState<File>();
-    const { edgestore} = useEdgeStore();
+    const { edgestore } = useEdgeStore();
+
+    console.log(value);
+    console.log(file)
 
     const uploadImgeHandler = async () => {
         if (file) {
             // 여기서 사진이 아니면 upload못하게 하면 좋긴한데 어짜피 나만 쓰니까 패스
-
+            console.log("동작씨발")
             const res = await edgestore.publicFiles.upload({
               file,
             });
@@ -38,31 +41,34 @@ const FileUpload = ({
 
     return (
         <div>
-            <SingleImageDropzone
+            {/* <SingleImageDropzone
                 width={200}
                 height={200}
                 value={file}
                 onChange={(file) => {
                     setFile(file);
+                    console.log("파일 업로드")
+                    
                 }}
-            />
-            <button
-                onClick={async () => {
-                if (file) {
-                    const res = await edgestore.publicFiles.upload({
-                    file,
-                    onProgressChange: (progress) => {
-                        // you can use this to show a progress bar
-                        console.log(progress);
-                    },
-                    });
-                    // you can run some server action or api here
-                    // to add the necessary data to your database
-                    console.log(res);
-                }
-                }}
-            >
-            </button>
+            /> */}
+<button
+        onClick={async () => {
+          if (file) {
+            const res = await edgestore.publicFiles.upload({
+              file,
+              onProgressChange: (progress) => {
+                // you can use this to show a progress bar
+                console.log(progress);
+              },
+            });
+            // you can run some server action or api here
+            // to add the necessary data to your database
+            console.log(res);
+          }
+        }}
+      >
+        Upload
+      </button>
         </div>
     )
 }
--- components/write/directWrite.tsx
+++ components/write/directWrite.tsx
@@ -21,18 +21,46 @@ import Tiptap from "../tiptap";
 
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useEdgeStore } from "@/lib/edgestore";
+import { SingleImageDropzone } from "../ui/singleImageDropZone";
 
 const formSchema = z.object({
     title : z
         .string()
-        .min(1, {message : "Title is required"})
-        .max(100, {message : "Title must be less than 100 characters"}),
+        .min(1, {message : "Title is required"}),
     content : z.string().min(1, {message : "Content is required"}),
     imageUrl : z.string().min(1, {message : "Image is required"}),
 })
 
 export default function DirectWrite() {
+
+    const [file, setFile] = useState<File>();
+    const { edgestore } = useEdgeStore();
+
+    const uploadImgeHandler = async () => {
+        console.log("실행")
+        if (file) {
+            // 여기서 사진이 아니면 upload못하게 하면 좋긴한데 어짜피 나만 쓰니까 패스
+            const res = await edgestore.publicFiles.upload({
+                file,
+            });
+
+            if(res.url) {
+                // url이 등록 되고 나서 imageUrl을 저장해야함.
+                form.reset((formValues) => ({
+                    ...formValues,
+                    imageUrl : res.url,
+                }))
+            }
+        }
+    }
+
+    useEffect(()=>{
+        if(file) {
+            uploadImgeHandler();
+        }
+    }, [file]);
 
     const form = useForm({
         resolver: zodResolver(formSchema),
@@ -43,37 +71,30 @@ export default function DirectWrite() {
         },
     })
 
-    const [content, setContent] = useState<string>('')
-    const handleContentChange = (reason: any) => {
-      setContent(reason)
+    const onActions = (values : z.infer<typeof formSchema>) => {
+        console.log(values);
     }
-
+    
     return (
         <div className="mt-8 mx-auto w-full max-w-3xl px-4">
             <div className="bg-white py-8 shadow rounded-lg px-10">
                 <h1 className="text-center text-2xl font-extrabold mb-10">
                     Create a Post!
                 </h1>
+                <div className="w-full flex justify-center">
+                <SingleImageDropzone
+                    width={200}
+                    height={200}
+                    value={file}
+                    onChange={(file) => {
+                        setFile(file);
+                    }}
+                />
+                </div>
                 <div>
                     <Form {...form}>
-                        <form className="space-y-8">
+                        <form onSubmit={form.handleSubmit(onActions)} className="space-y-8">
                             <div className="space-y-8 px-6">
-                            <FormField
-                                control={form.control}
-                                name="imageUrl"
-                                render={({field})=>(
-                                    <FormItem>
-                                        <FormControl>
-                                            <div className="flex justify-center">
-                                                <FileUpload
-                                                    value={field.value}
-                                                    onChange={field.onChange}
-                                                />
-                                            </div>
-                                        </FormControl>
-                                    </FormItem>
-                                )}
-                            />
                             <FormField
                                 control={form.control}
                                 name="title"
@@ -107,9 +128,7 @@ export default function DirectWrite() {
                             />
                             </div>
                             <div className="flex justify-end">
-                                <Button 
-                                    className="justify-end"
-                                    variant="custom">
+                                <Button className="justify-end"variant="custom">
                                     Submit
                                 </Button>
                             </div>
